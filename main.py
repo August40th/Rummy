d2061757f4f7e984b3fffb3c0a0213e96f072d1c
August40th <40985705+August40th@users.noreply.github.com>
2023-09-21 22:57:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -63,19 +63,104 @@ class Player:
         self.buys = 0
         self.laid_down = False
         self.cumulative_score = 0
-
-    def draw_card(self, draw_pile):
+        
+    def deal_card(self):
         if draw_pile:
             drawn_card = draw_pile.pop()
             self.hand.append(drawn_card)
+    
+    def draw_card(self, draw_pile, discard_pile):
+        print("\n" + self.name + "'s turn to draw") 
+        self.hand.sort(key=lambda card: ranks.index(card[0]), reverse=True)
+        print(self.name, "'s Hand:", [f"{rank}{suit}" for rank, suit in self.hand])
+    
+        drawn_card = None
+        cards_match = []
+    
+        if discard_pile:
+            top_card = discard_pile[-1]
+            cards_match = [card for card in self.hand if card[0] == top_card[0]]
         else:
-            reshuffle(draw_pile, discard_pile)
+            top_card = None
+    
+        if self.laid_down and discard_pile:
+            for other_player in players:
+                for card in other_player.hand:
+                    if card[0] == top_card[0]:
+                        drawn_card = discard_pile.pop()
+                        self.hand.append(drawn_card)
+                        print(self.name + " drew", f"{drawn_card[0]}{drawn_card[1]}", "from the discard pile to play in the set")
+                        return
+    
+            if top_card and ranks.index(top_card[0]) > ranks.index(self.hand[-1][0]):
+                drawn_card = discard_pile.pop()
+                self.hand.append(drawn_card)
+                print(self.name + " drew", f"{drawn_card[0]}{drawn_card[1]}", "from the discard pile because it's lower")
+    
+        elif not self.laid_down and discard_pile:
+            if len(cards_match) >= 2:
+                cards_match.clear()
+                drawn_card = discard_pile.pop()
+                self.hand.append(drawn_card)
+                print(self.name + " drew", f"{drawn_card[0]}{drawn_card[1]}", "from the discard pile because of a matching pair for a set")
+    
+        if not drawn_card:
             drawn_card = draw_pile.pop()
             self.hand.append(drawn_card)
-
-    def discard_card(self, card, discard_pile):
-        self.hand.remove(card)
-        discard_pile.append(card)
+            print(self.name + " drew", f"{drawn_card[0]}{drawn_card[1]}", "from the deck")
+    
+        cards_match.clear()
+        
+    def discard_card(self, discard_pile):
+        discard_choice = None
+        pairs = []
+        sets = []
+        potential_discard = []
+    
+        if self.laid_down:
+            self.hand.sort(key=lambda card: ranks.index(card[0]))
+            rank_cards = [card for card in self.hand]
+            potential_discard.append(rank_cards)
+        else:
+            for rank in ranks:
+                self.hand.sort(key=lambda card: ranks.index(card[0]))
+                rank_cards = [card for card in self.hand if card[0].startswith(rank)]
+                if len(rank_cards) == 1:
+                    potential_discard.append(rank_cards)
+            if not potential_discard:
+                for rank in ranks:
+                    rank_cards = [card for card in self.hand if card[0].startswith(rank)]
+                    if len(rank_cards) == 4 or len(rank_cards) == 5:
+                        potential_discard.append(rank_cards)
+            if not potential_discard:
+                for rank in ranks:
+                    rank_cards = [card for card in self.hand if card[0].startswith(rank)]
+                    if len(rank_cards) == 2:
+                        potential_discard.append(rank_cards)
+            if not potential_discard:
+                for rank in ranks:
+                    rank_cards = [card for card in self.hand if card[0].startswith(rank)]
+                    if len(rank_cards) == 3:
+                        potential_discard.append(rank_cards)
+    
+        last_check = potential_discard
+    
+        if len(potential_discard) == 0 and len(self.hand) > 1:
+            potential_discard = last_check
+            discard_choice = self.hand[-1]
+        else:
+            discard_choice = potential_discard[0][0]
+    
+        # Remove the card from the player's hand and discard it
+        if len(self.hand) > 1:
+            self.hand.remove(discard_choice)
+            discard_pile.append(discard_choice)
+            print(self.name + " discarded", f"{discard_choice[0]}{discard_choice[1]}")
+        else:
+            for card in self.hand:
+                discard_pile.append(card)
+                self.hand.remove(card)
+                print(self.name + " discarded", f"{card[0]}{card[1]}")
 
     def buy_from_discard(self, discard_pile, draw_pile):
         if self.buys < 3:
@@ -127,7 +212,7 @@ for round_number in range(1, 7):
     c = 1
     while c < 11:
         for player in players:
-            player.draw_card(draw_pile)
+            player.deal_card()
         c += 1
 
     # Display each player's initial hand
@@ -135,6 +220,11 @@ for round_number in range(1, 7):
         player.hand = sorted(player.hand, key=lambda card: (card[1], card[0]))
         sorted_hand = [f"{rank}{suit}" for rank, suit in player.hand]  # Format cards as rank and suit without space
         print(f"{player.name}'s Hand: {sorted_hand}")
+        player.draw_card(draw_pile, discard_pile)
+        player.hand = sorted(player.hand, key=lambda card: (card[1], card[0]))
+        sorted_hand = [f"{rank}{suit}" for rank, suit in player.hand]  # Format cards as rank and suit without space
+        print(f"{player.name}'s Hand: {sorted_hand}")
+        player.discard_card(discard_pile)
 
     # End round
     for player in players:
